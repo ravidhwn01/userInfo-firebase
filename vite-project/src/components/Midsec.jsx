82765d68bb886457a2ad95c2a81f5b65dd6fc80d
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-11-29 22:37:04 +0530

    added some pages like error, services and aboutus

--- vite-project/src/components/Midsec.jsx
+++ vite-project/src/components/Midsec.jsx
@@ -1,9 +1,29 @@
-import React from "react";
-
+import React , { useState } from "react";
+import workapi from "../api/Workapi";
 function Midsec() {
+    const [workData, setWorkData] = useState(workapi);
+  console.log(workData);
   return (
     <>
-      <h1>how it works</h1>
+      <section>
+        <div className="work-container container">
+          <h1 className="main-heading text-center">How does it Work</h1>
+          <div className="row">
+            {workData.map((curElem) => {
+              const { id, logo, title, info } = curElem;
+              return (
+                <>
+                  <div className="col-12 col-lg-4 text-center work-container-subdiv">
+                    <i className={`fontawesome-style ${logo}`}></i>
+                    <h2 className="sub-heading">{title}</h2>
+                    <p className="main-hero-para w-100">{info}</p>
+                  </div>
+                </>
+              );
+            })}
+          </div>
+        </div>
+      </section>
     </>
   );
 }
--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -5,35 +5,35 @@ function Navbar() {
   return (
    <>
    <section className='navbar-bg' > 
-    <nav class="navbar navbar-expand-lg">
-  <div class="container">
-    <a class="navbar-brand" href="#">UserInfo</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" 
+    <nav className="navbar navbar-expand-lg">
+  <div className="container">
+    <a className="navbar-brand" href="#">UserInfo</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" 
     onClick={()=>{
         setShow(!show)
     }}
     >
-      <span class="navbar-toggler-icon"></span>
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class={`collapse navbar-collapse ${show? "show":"" } `}id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="#">Home</a>
+    <div className={`collapse navbar-collapse ${show? "show":"" } `}id="navbarSupportedContent">
+      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link" aria-current="page" href="#">Home</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/about">About</a>
+        <li className="nav-item">
+          <a className="nav-link" href="/about">About</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/services">Services</a>
+        <li className="nav-item">
+          <a className="nav-link" href="/services">Services</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/contact">Contact</a>
+        <li className="nav-item">
+          <a className="nav-link" href="/contact">Contact</a>
         </li>
        
       </ul>
-      <form class="d-flex" role="search">
-        <button class="btn   btn-style " type="submit">Sign up</button>
-        <button class="btn   btn-style btn-style-border " type="submit">Log in</button>
+      <form className="d-flex" role="search">
+        <button className="btn   btn-style " type="submit">Sign up</button>
+        <button className="btn   btn-style btn-style-border " type="submit">Log in</button>
       </form>
     </div>
   </div>
