a469a2a47105bfa8e7cf11f46d37d4ef2033345e
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-12-03 23:36:45 +0530

    inserted data into firebase

--- firebase curd operation/index.js
+++ firebase curd operation/index.js
@@ -1,14 +1,18 @@
- "use strick"
- const express = require("express")
- const cors = require("cors")
- const bodyParser = require("body-parser")
- const config = require("./config")
- const app  = express();
- app.use(express.json())
- app.use(cors())
- app.use(bodyParser.json())
+'use strict';
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const config = require('./config');
+const studentRoutes = require('./routes/studentRoute');
 
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/api', studentRoutes.routes);
 
  app.listen(config.port,()=>{
-    console.log(`App is running at port http://localhost${config.port}`)
- })
+    console.log(`App is running at port http://localhost:${config.port}`)
+ }) 
